1a8e927de6f166128ffae0c08893b616f6de4d51
Tiago Dantas <tiago.freire@enext.com.br>
2022-03-01 18:24:53 -0300

    Checkout Stripe

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,52 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          // Informação que a aplicação vai puxar do Github
+          scope: 'read:user',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return true;
+      }
+    },
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { stripe } from '../services/stripe';
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1K4wbhKfjZ7i6PHC95pcqrnJ')
 
   const product = {
@@ -46,11 +46,12 @@ export const getServerSideProps: GetServerSideProps = async () => {
       style: 'currency',
       currency: 'USD',
     }).format(price.unit_amount / 100),
-  }
+  };
 
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 }
